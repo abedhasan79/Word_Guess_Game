ce5bfc128feca8b84469156fb8cb0e9f9cb77fbf
Abed Hasan <abedhasan79@gmail.com>
2022-09-18 22:10:14 -0400

    shows win loss

--- assets/script.js
+++ assets/script.js
@@ -3,43 +3,48 @@ let guessWord = document.getElementById("word");
 let timer = document.getElementById("timer");
 let inputBox = document.getElementById("inputBox");
 let keyGenWord = document.getElementById("keyGenWord");
+let winGame = document.getElementById("win");
+let lossGame = document.getElementById("loss");
 
 
-timer.textContent = "Click Start!"
+
+localStorage.setItem("2", lossGame);
+// timer.textContent = "Click Start Game Button!"
 
 let month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
-
-
-let randomItem = month[Math.floor(Math.random() * month.length)];
-console.log(randomItem);
-let arrayAnswer = [];
-for (let i = 0; i < randomItem.length; i++) {
-    arrayAnswer[i] = "_";
-}
-
-
-
+let win = localStorage.getItem("win");
+let loss= localStorage.getItem("loss");
 
 function startGame() {
+    let randomItem = month[Math.floor(Math.random() * month.length)];
+    console.log(randomItem);
+    let arrayAnswer = [];
+    for (let i = 0; i < randomItem.length; i++) {
+        arrayAnswer[i] = "_";
+    }
 
-    inputBox.addEventListener('keypress', function(e){
-        inputBox.value=e.key;
-        
+    inputBox.addEventListener('keypress', function (e) {
+        inputBox.value = e.key;
+
         for (let j = 0; j < randomItem.length; j++) {
             if (e.key == randomItem.charAt(j)) {
-            arrayAnswer[j]=e.key;
-            let arrayToString = arrayAnswer.join(" ");
-            guessWord.innerHTML = arrayToString;
-        }
-        
-        if(!arrayAnswer.includes("_")){
-            timer.setAttribute("style","display:none");
-            inputBox.setAttribute("style", "display:none");
-            keyGenWord.textContent = "You Win!";
-        }
+                arrayAnswer[j] = e.key;
+                let arrayToString = arrayAnswer.join(" ");
+                guessWord.innerHTML = arrayToString;
+            }
 
-        
-    }
+            if (!arrayAnswer.includes("_")) {
+                timer.setAttribute("style", "display:none");
+                inputBox.setAttribute("style", "display:none");
+                keyGenWord.textContent = "You Win!";
+                win++;
+                winGame.textContent ="WIN: "+ win;
+                localStorage.setItem("win", win);
+                
+            }
+
+
+        }
     })
 
 
@@ -51,7 +56,14 @@ function startGame() {
             clearInterval(timeInterval);
             timer.textContent = "TIMES UP";
             inputBox.setAttribute("style", "display:none");
-            
+            if (arrayAnswer.includes("_")) {
+                inputBox.setAttribute("style", "display:none");
+                keyGenWord.textContent = "You LOST!";
+                loss++;
+                lossGame.textContent="LOSS: "+loss;
+                localStorage.setItem("loss", loss);
+            }
+
         }
     }, 1000)
 
